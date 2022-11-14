bac86f1933ac7aea19ef793741233fc7c78723df
root <root@Audrey-Ann.localdomain>
2022-11-14 21:34:17 +0000

    tracking all file
    smart contract with several dependencies and libs

---
+++ deploy.js
@@ -0,0 +1,35 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  //compile separately
+  //compile in our code
+  // http://127.0.0.1:7545
+
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  const wallet = new ethers.Wallet(
+    "9a21c73eef96fd418c1eabde7adae5eb63c460310379f7d02813a5e5640001ca",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
+
+main()
+  .then(() => Process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
